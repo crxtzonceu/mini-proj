440ac41e0404b13df791924e09f7f2da4788b990
crxtzonceu <zamanthaquijano@gmail.com>
2022-10-24 14:35:32 +0800

    break

--- chrome-extension/code.js
+++ chrome-extension/code.js
@@ -1,3 +1,19 @@
 let myList = []
-let inputEl = document.getElementById("input-el")
-let 
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn").addEventListener("click", saveInput)
+const listEl = document.getElementById("list-el")
+
+
+function saveInput() {
+  myList.push(inputEl.value)
+  inputEl.value = ""
+  renderList()
+}
+
+function renderList() {
+  let listItems = ""
+  for(let i = 0;i < myList.length;i++) {
+    listItems += `<li>${myList[i]}</li>`
+  }
+  listEl.innerHTML = listItems
+}
